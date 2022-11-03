657e2d1657ebb9c37e9718e4ec38a28cd3bd23dd
zxcnevermore <lilskies1@mail.ru>
2022-11-03 02:19:46 +0200

    create api

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 5000
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({info: 'Main endpoint'})
+})
+
+app.get('/anime', db.getAnime)
+
+app.listen(port, () => {
+  console.log(`Server start on port + ${port}`)
+})
---
+++ api/queries.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+})
+
+
+const getAnime = (request, response) => {
+  const { page, size, title} = request.query
+  if(page && size) {
+    pool.query(`SELECT * FROM anime WHERE title ILIKE '%${title}%'
+    ORDER BY id ASC LIMIT ${size} OFFSET ((${page} - 1) * ${size})`
+    ,(error, results) => {
+      if(error) {
+        throw error
+      }
+    pool.query('SELECT * from anime', (err, res) => {
+      if(err) {
+        throw err
+      }
+      response.status(200).json({data:results.rows, totalData:res.rowCount})
+    })
+    })
+  }
+  else {
+    response.status(411).json({message: "Params page,size required"})
+  }
+}
+
+
+module.exports = {
+  getAnime
+}
